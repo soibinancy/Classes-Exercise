829c97d3cb61e82deb4c16ebd67b3af0f86f4d98
Soibi Nancy <39niches@gmail.com>
2024-05-12 19:07:15 -0500

    classes first exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Corolla";
+            myCar.Year = 2023;
+
+            Console.WriteLine($"My car is a {myCar.Make} {myCar.Model}, made in the year {myCar.Year}.");
         }
     }
 }
